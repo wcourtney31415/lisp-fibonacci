08135181ef8590770dc23a6463e98253ef86a0ac
wcourtney31415 <wcourtney31415@gmail.com>
2021-09-09 01:59:55 -0400

    Initial Commit.

---
+++ main.lisp
@@ -0,0 +1,35 @@
+
+( defun fibRecursive 
+    ( myList myCount ) 
+    ( let*
+        ( 
+            ( a 
+                (nth 0 myList)
+)
+            ( b 
+                (nth 1 myList)
+)
+            ( sum 
+                (+ a b)
+)
+            ( nextList 
+                ( cons sum myList ) 
+)
+)
+        ( 
+if
+            (= 
+                (length myList) myCount)  
+            ( print 
+                (reverse myList) ) 
+            ( fibRecursive nextList myCount )
+)
+)
+)
+
+( defun fibonacci 
+    (myCount) 
+    ( fibRecursive 
+        (List 1 0) myCount ) )
+
+(fibonacci 25)
